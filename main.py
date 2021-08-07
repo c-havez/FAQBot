a90657784a880057e810ae6d103aba29a84953b8
AaronRivest <88506408+c-havez@users.noreply.github.com>
2021-08-07 03:30:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import discord
+import os
+import requests
+from replit import db
+#from keep_alive import keep_alive
+
+client = discord.Client()
+
+# def FAQ(question):
+#   QinDB = find_question(question)
+#   if QinDB == True:
+#     AinDB = find_answer(question)
+#   else:
+#     update_questions(question, answer)
+
+# def find_question(question):
+#   for i in questions:
+#     if question == i:
+#       return True
+#   return False
+
+# def update_questions():
+#   yes = "y"
+#   #if "questions" in db.key():
+#     #questions = db["questions"]
+#     #questions.append(question_message)
+
+# def find_answer(question):
+
+
+#def update_question(question):
+  # if "question" in db.keys():
+
+
+
+@client.event
+async def on_ready():
+  print('We have logged in as user {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  
+  if message.content.startswith('?FAQQ'):
+    question = message.content[4:]
+    inDB = get_question(question)
+    if inDB == True:
+      answer
+    await message.channel.send(question)
+
+
+#keep_alive()
+client.run(os.getenv('TOKEN'))
